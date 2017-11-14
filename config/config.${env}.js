39ef732a44d8e769a4cd0b343e2011b15b1c7ff2
bruce <riskgod.chen@pwc.com>
2017-11-14 18:17:20 +0800

    add mysql

---
+++ config/config.${env}.js
@@ -0,0 +1,12 @@
+
+exports.mysql = {
+    client: {
+      host: 'localhost',
+      port: '3306',
+      user: 'admin',
+      password: '1qaz2wsx',
+      database: 'law',
+    },
+    app: true,
+    agent: false,
+};
